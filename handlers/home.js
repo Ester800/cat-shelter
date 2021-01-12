472c63403de6ab08605e3fe6d03af295eb3b2fda
McKay Stevenson <mckay800@gmail.com>
2021-01-11 21:56:25 -0500

    Monday's work

--- handlers/home.js
+++ handlers/home.js
@@ -1,8 +1,10 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-//const cats = require('../data/cats.json');
-//const breeds = require('../data/breeds.json');
+const qs = require('querystring');
+const formidable = require('formidable');
+const cats = require('../data/cats.json');
+const breeds = require('../data/breeds.json');
 
 
 module.exports = (req, res) => {
@@ -41,7 +43,9 @@ module.exports = (req, res) => {
             const index = fs.createReadStream(filePath);
     
             index.on('data', (data) => {
-                res.write(data);
+                let catBreedPlaceholder = breeds.map((breed) => `<option value="${breed}">${breed}</option>`);
+                let modifiedData = data.toString().replace('{{catBreeds}}', catBreedPlaceholder);
+                res.write(modifiedData);
             });
     
             index.on('end', () => {
@@ -84,10 +88,32 @@ module.exports = (req, res) => {
     } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
         let formData = '';
         req.on('data', (data) => {
-            console.log('the breed form data is ', data);
+            //console.log('the breed form data is ', data.toString());
             formData += data;
-            console.log('the new data is ', formData)
+            //console.log('the new data is ', formData)
+            let parsedData = qs.decode(formData);
+            console.log("the parsed data is", parsedData.breed);
+    
+
+        fs.readFile('./data/breeds.json', 'utf8', (err, data) => {
+            if(err) {
+                console.log(err);
+                return
+            }
+            
+            let currentBreeds = JSON.parse(data);
+            currentBreeds.push(parsedData.breed);
+            let updatedBreeds = JSON.stringify(currentBreeds);
+            console.log('the updated ready to save data is', updatedBreeds);
+
+            fs.writeFile('./data/breeds.json', updatedBreeds, 'utf-8', () => {
+                console.log('The breed was uploaded successfully...');
+            });
+
+            res.writeHead(301, { location: '/' });
+            res.end();
         });
+    });
  
     } else {
         return true;
